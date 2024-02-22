d992bbcfb2e73db46377d05b33535ef1a8b1fa69
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-02-21 20:38:48 -0600

    Add files via upload

---
+++ operadoreslogicos4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    printf("****** AND ******\n");
+    printf(" true && true: %d\n", ( 1 && 1));
+    printf(" true && false: %d\n", (1 && 0));
+    printf(" false && true; %d\n", (0 && 1));
+    printf(" false && false; %d\n", (0 && 0));
+
+    printf("****** 0R ******\n");
+    printf(" true || true: %d\n", ( 1 || 1));
+    printf(" true || false: %d\n", (1 || 0));
+    printf(" false || true; %d\n", (0 || 1));
+    printf(" false || false; %d\n", (0 || 0));
+
+    printf("****** XOR ******\n");
+    printf(" true ^ true: %d\n", ( 1 ^ 1));
+    printf(" true ^ false: %d\n", (1 ^ 0));
+    printf(" false ^ true; %d\n", (0 ^ 1));
+    printf(" false ^ false; %d\n", (0 ^ 0));
+
+    return 0;
+
+  
+}
