69e57baa7891b454747cb12647922ff3d98f813f
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-02-23 16:20:17 -0600

    Add files via upload

---
+++ incredecremento1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int var = 1;
+    int con = 1; 
+
+    printf("%d", var++);
+    printf("\n");
+    printf("%d", var);
+    printf("\n");
+    printf("%d",con--);
+    printf("\n");
+    printf("%d",con);
+    printf("\n");
+    
+    return 0;
+
+}
